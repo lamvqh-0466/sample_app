fa6a47b6acf3f32d32569b3253fd7558d3754ea8
lam-vqh0466 <vu.quang.hoang.lam@sun-asterisk.com>
2025-02-26 16:05:29 +0700

    chapter 7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+    return if @user
+
+    flash[:danger] = t "user_not_found"
+    redirect_to static_pages_home_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:success] = Settings.user.create_successfull
+      redirect_to @user
+    else
+      flash.now[:danger] = t "submit_fail"
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def users_params
+    params.require(:user).permit(User::ATTRIBUTES)
+  end
+end
