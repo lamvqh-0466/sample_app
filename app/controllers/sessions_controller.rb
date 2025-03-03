76d5ac553fa9c6a4d75aebc03f30963944be66c7
lam-vqh0466 <vu.quang.hoang.lam@sun-asterisk.com>
2025-03-03 10:59:34 +0700

    charpter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,13 @@
 class SessionsController < ApplicationController
+  before_action :load_user, only: :create
+
   def new; end
 
   def create
-    @user = User.find_by email: params.dig(:session, :email)&.downcase
-    if @user&.authenticate params.dig(:session, :password)
-      log_in @user
-      flash[:success] = t "login_successfull"
-      redirect_to user_path(id: @user.id), status: :see_other
+    if @user&.authenticate(params.dig(:session, :password))
+      handle_login
     else
-      flash.now[:danger] = t "invalid_email_ password_combination"
-      render :new
+      handle_failed_login
     end
   end
 
@@ -17,5 +15,31 @@ class SessionsController < ApplicationController
     log_out
     flash[:success] = t "logout_successfull"
     redirect_to static_pages_home_path
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by(email: params.dig(:session, :email)&.downcase)
+  end
+
+  def handle_login
+    log_in @user
+    handle_remember_me
+    flash[:success] = t "login_successfull"
+    redirect_to user_path(id: @user.id), status: :see_other
+  end
+
+  def handle_failed_login
+    flash.now[:danger] = t "invalid_email_password_combination"
+    render :new
+  end
+
+  def handle_remember_me
+    if params.dig(:session, :remember_me) == "1"
+      remember(@user)
+    else
+      forget(@user)
+    end
   end
 end
